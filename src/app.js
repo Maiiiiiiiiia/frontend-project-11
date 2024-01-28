c09298184bbf06e9673fea29f2a613e4869519c9
Maia Kulieva <hi.maiakulieva@gmail.com>
2024-01-28 19:04:39 +0400

    refactoring

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
 import onChange from "on-change";
 import render from "./view";
 
-
 export default () => {
 // model
-const state = {
+  const state = {
     link: '',
     validLinks: [],
   };
 
-const watchedState = onChange(state, () => {
+  const watchedState = onChange(state, () => {
     render(state.link, state);
-});
+  });
 
-// controller
-const form = document.querySelector('.rss-form');
-form.addEventListener('submit', (e) => {
+  // controller
+  const form = document.querySelector('.rss-form');
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const url = formData.get('url');
     watchedState.link = url;
-});
+  });
 };
--- src/view.js
+++ src/view.js
@@ -1,34 +1,34 @@
 import * as Yup from 'yup';
 
 const render = (url, state) => {
-    const schema = Yup.string().trim().required().url().notOneOf(state.validLinks);
-    const feedback = document.querySelector('.feedback');
-    const inputText = document.querySelector('#url-input');
-    const form = document.querySelector('.rss-form');
+  const schema = Yup.string().trim().required().url().notOneOf(state.validLinks);
+  
+  const feedback = document.querySelector('.feedback');
+  const inputText = document.querySelector('#url-input');
+  const form = document.querySelector('.rss-form');
 
-    schema.validate(url)
+  schema.validate(url)
     .then(() => {
-        inputText.classList.remove('is-invalid');
-        feedback.classList.replace('text-danger', 'text-success');
-        feedback.textContent = 'RSS успешно загружен';
-        state.validLinks.push(url);
-        console.log(state.validLinks);
-        form.reset();
-        inputText.focus();
+      state.validLinks.push(url);
+      inputText.classList.remove('is-invalid');
+      feedback.classList.replace('text-danger', 'text-success');
+      feedback.textContent = 'RSS успешно загружен';
+      form.reset();
+      inputText.focus();
+    //   console.log(state.validLinks);
     })
     .catch(() => {
+      if (state.validLinks.includes(url)) {
         inputText.classList.add('is-invalid');
         feedback.classList.replace('text-success', 'text-danger');
-        feedback.textContent = 'Ссылка должна быть валидным URL';
-        console.log(state.validLinks);
-        if (state.validLinks.includes(url)) {
-            inputText.classList.add('is-invalid');
-            feedback.classList.replace('text-success', 'text-danger');
-            feedback.textContent = 'RSS уже существует'; 
-            console.log('RSS уже существует');
-        };
-    })
+        feedback.textContent = 'RSS уже существует';
+        // console.log('RSS уже существует');
+      }
+      inputText.classList.add('is-invalid');
+      feedback.classList.replace('text-success', 'text-danger');
+      feedback.textContent = 'Ссылка должна быть валидным URL';
+      // console.log(state.validLinks);
+    });
 };
 
 export default render;
-
