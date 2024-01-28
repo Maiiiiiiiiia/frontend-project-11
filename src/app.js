86d22e20faa9f5264d571673b4f2f94ddc0abecf
Maia Kulieva <hi.maiakulieva@gmail.com>
2024-01-28 15:04:58 +0400

    add mvc model and chenged styles

--- src/app.js
+++ src/app.js
@@ -3,61 +3,49 @@ import * as Yup from 'yup';
 
 
 export default () => {
-
+// model
 const state = {
-        validLinks: [],
+    link: '',
+    validLinks: [],
   };
 
-const form = document.querySelector('.rss-form');
-const inputText = document.querySelector('#url-input');
-const sendButton = document.querySelector('[type="submit"]');
-const feedback = document.querySelector('.feedback');
-
-// const render = () => {};
-// const watchedState = onChange(state, render);
-
-// Второй вариант валидации:
-// const schema = yup.object().shape({
-//     url: yup.string().required().url('Ссылка должна быть валидным URL').notOneOf(validLinks),
-//   });
-// const validate = (url) => {
-//     try {
-//         schema.validateSync(url);
-//         return {};
-//     //return true;
-//     console.log('валидно');
-//     } catch {
-//     //return false;
-//     console.log('не валидно')
-//     }
-// };
-
-
-const isValidLinks = (url) => {
-try {
-    // Yup.string().required().url().validate(url);
-    Yup.string().required().url().notOneOf(validLinks);
-    //return true;
-    console.log('валидно');
-} catch {
-    //return false;
-    console.log('не валидно')
-}
+// view
+// const sendButton = document.querySelector('[type="submit"]');
+const render = (url) => {
+    const schema = Yup.string().trim().required().url().notOneOf(state.validLinks);
+    const feedback = document.querySelector('.feedback');
+    const inputText = document.querySelector('#url-input');
+    schema.validate(url)
+    .then(() => {
+        console.log('WATCHEDSTATE IS WORK');
+        inputText.classList.remove('is-invalid');
+        feedback.classList.replace('text-danger', 'text-success');
+        feedback.textContent = 'RSS успешно загружен';
+        state.validLinks.push(url);
+        console.log(state.validLinks);
+    })
+    .catch(() => {
+        inputText.classList.add('is-invalid');
+        feedback.classList.replace('text-success', 'text-danger');
+        feedback.textContent = 'Ссылка должна быть валидным URL';
+        if(state.validLinks.includes(url)) {
+            feedback.classList.replace('text-success', 'text-danger');
+            feedback.textContent = 'RSS уже существует'; 
+        }
+    })
 };
 
+// view
+const watchedState = onChange(state, () => {
+    render(state.link);
+});
+
+// controller
+const form = document.querySelector('.rss-form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const url = formData.get('url');
-    const isValidLink = isValidLinks(url);
-
-if (isValidLink) {
-    state.validLinks.push(isValidLink);
-    feedback.classList.replace('text-danger', 'text-success');
-    feedback.textContent = 'RSS успешно загружен';
-} else 
-    feedback.classList.replace('text-success', 'text-danger');
-    feedback.textContent = 'Ссылка должна быть валидным URL';
+    watchedState.link = url;
 });
-
 };
